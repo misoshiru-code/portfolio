f7a15899930cfd074105399519b591df3ad3bc0d
misoshiru <voushi@icloud.com>
2021-04-20 07:37:58 +0900

    バージョン管理開始

---
+++ dest/js/index.js
@@ -0,0 +1,115 @@
+//JavaScript
+
+const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  
+  // 画面のサイズ変動があった時に高さを再計算する
+  window.addEventListener('resize', setFillHeight);
+  
+  // 初期化
+  setFillHeight();
+
+  
+
+// メニューの開閉
+$(function () {
+    var duration = 1000;
+
+    $('.menu_trigger').on('click', function () {
+        $('.nav_bg').toggleClass('open');
+
+        if ($('.nav_bg').hasClass('open')) {
+            $('.nav_bg').stop(true).animate({
+                left: '0'
+            }, duration, 'easeOutExpo');
+        } else {
+            $('.nav_bg').stop(true).animate({
+                left: '100%'
+            }, duration, 'easeOutExpo');
+        };
+    });
+});
+
+/*
+// blog.htmlのサイト内検索の開閉
+$(function () {
+    var duration = 1000;
+
+    $('.icon').on('click', function () {
+        $('.search_input_wrap').toggleClass('open2');
+
+        if ($('.search_input_wrap').hasClass('open2')) {
+            $('.search_input_wrap').stop(true).animate({
+                width: '240px'
+            }, duration, 'easeOutExpo');
+            $('.icon').stop(true).animate({
+                left: '38%'
+            }, duration, 'easeOutExpo');
+        } else {
+            $('.search_input_wrap').stop(true).animate({
+                width: '0'
+            }, duration, 'easeOutExpo');
+            $('.icon').stop(true).animate({
+                left: '49%'
+            }, duration, 'easeOutExpo');
+        };
+    });
+});
+*/
+
+// プログレス表示
+$(function () {
+
+    imagesProgress();
+
+    function imagesProgress() {
+
+        var $container = $('#progress'),
+            $progressBar = $container.find('.progress-bar'),
+            $progressText = $container.find('.progress-text'),
+
+            imgLoad = imagesLoaded('body'),
+            imgTotal = imgLoad.images.length,
+
+            imgLoaded = 0,
+            current = 0,
+
+            progressTimer = setInterval(updateProgress, 1000 / 60);
+
+        imgLoad.on('progress', function () {
+            imgLoaded++;
+        });
+
+        function updateProgress() {
+
+            var target = (imgLoaded / imgTotal) * 100;
+
+            current += (target - current) * 0.1;
+
+            $progressBar.css({ width: current + '%' });
+            $progressText.text(Math.floor(current) + '%');
+
+            if (current >= 100) {
+                clearInterval(progressTimer);
+                // $container.addClass('progress-complete');
+                // $progressBar.add($progressText)
+                //     .delay(500)
+                //     .animate({ opacity: 0 }, 250, function () {
+                //         $container.animate({ top: '-100%' }, 1000, 'easeInOutQuint');
+                //     });
+                $container
+                    .delay(500)
+                    .animate({ opacity: 0 }, 1000, function () {
+                        $container.css({ top: '-100%' });
+                    });
+            }
+
+            if (current > 99.9) {
+                current = 100;
+            }
+        }
+    }
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,192 @@
+const gulp = require("gulp");//本体
+const del = require('del');//削除
+const plumber = require("gulp-plumber");//エラーで強制終了の無効
+const notify = require("gulp-notify"); // エラー発生時のアラート出力
+//ejs
+const rename = require("gulp-rename");//名前変更
+const ejs = require("gulp-ejs");//ejsコンパイル
+const replace = require("gulp-replace");//不要スペースの削除
+//sass
+const sass = require('gulp-sass');//sassコンパイル
+const browserSync = require('browser-sync');//ブラウザリロード
+const autoprefixer = require('gulp-autoprefixer');//ベンダープレフィックス自動付与
+const postcss = require("gulp-postcss");//css-mqpackerを使うために必要
+const mqpacker = require('css-mqpacker');//メディアクエリをまとめる
+const sassGlob = require('gulp-sass-glob');
+//画像圧縮
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+
+
+// ファイルパス：コンパイル前
+const srcEjsFiles = './src/ejs/**/*.ejs';
+const srcEjsPartial = '!./src/ejs/**/_*.ejs';
+const srcScssFiles = './src/scss/**/*.scss';
+const srcJsFiles = './src/js/**/*.js'
+const srcImgFiles = './src/img/**/*'
+const srcImgFileType = '{jpg,jpeg,png,gif,svg}';
+const srcMovFiles = './src/mov/**/*'
+
+// ファイルパス：コンパイル後
+const destDir = './dest/';
+const destHtmlFiles = './dest/*.html';
+const destIndexHtml = 'index.html';
+const destCssDir = './dest/css';
+const destCssFiles = './dest/css/*.css';
+const destJsDir = './dest/js';
+const destJsFiles = './dest/js/*.js';
+const destImgDir = './dest/img';
+const destImgFiles = './dest/img/*';
+const destMovDir = './dest/mov';
+const destMovFiles = './dest/mov/**/*'
+const destFiles = './dest/**/*';
+
+
+// EJSコンパイル
+const compileEjs = (done) => {
+    gulp.src([srcEjsFiles, srcEjsPartial])
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError('Error:<%= error.message %>')
+            }))
+        .pipe(ejs())
+        .pipe(ejs({}, {}, { ext: '.html' }))
+        .pipe(rename({ extname: '.html' }))
+        .pipe(replace(/^[ \t]*\n/gmi, ''))
+        .pipe(gulp.dest(destDir));
+    done();
+};
+
+// sassコンパイル
+const compileSass = (done) => {
+    gulp.src(srcScssFiles)
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError('Error:<%= error.message %>')
+            }))
+        .pipe(sassGlob())
+        .pipe(sass({ outputStyle: 'expanded' }))//圧縮せずにcss出力
+        .pipe(autoprefixer(TARGET_BROWSERS))// ベンダープレフィックス自動付与
+        .pipe(postcss([mqpacker()])) // メディアクエリをまとめる
+        .pipe(gulp.dest(destCssDir));
+    done();
+};
+
+//ベンダープレフィックスを付与する条件
+const TARGET_BROWSERS = [
+    'last 2 versions',//各ブラウザの2世代前までのバージョンを担保
+    'ie >= 11'//IE11を担保
+];
+
+// jsコンパイル
+const compileJs = (done) => {
+    gulp.src(srcJsFiles)
+        .pipe(gulp.dest(destJsDir));
+    done();
+};
+
+// 動画ファイルコピー
+const copyMov = (done) => {
+    gulp.src(srcMovFiles)
+        .pipe(gulp.dest(destMovDir));
+    done();
+};
+
+// リロードするhtml
+const reloadFile = (done) => {
+    browserSync.init({
+        server: {
+            baseDir: destDir,
+            index: destIndexHtml,
+        },
+    });
+    done();
+};
+
+// リロード設定
+const reloadBrowser = (done) => {
+    browserSync.reload();
+    done();
+};
+
+// 画像圧縮
+const minifyImage = (done) => {
+    gulp.src(srcImgFiles + srcImgFileType)
+        .pipe(imagemin(
+            [
+                pngquant({ quality: [.65, .80], speed: 1 }),
+                mozjpeg({ quality: 80 }),
+                imagemin.svgo(),
+                imagemin.gifsicle()
+            ]
+        ))
+        .pipe(gulp.dest(destImgDir));
+    done();
+};
+
+// destフォルダのファイル削除
+const clean = (done) => {
+    del([destFiles, '!' + destCssDir, '!' + destJsDir, '!' + destImgDir, '!' + destMovDir]);
+    done();
+};
+
+// HTMLファイル削除
+const htmlClean = (done) => {
+    del([destHtmlFiles]);
+    done();
+};
+
+// CSSファイル削除
+const cssClean = (done) => {
+    del([destCssFiles]);
+    done();
+};
+
+// JavaScriptファイル削除
+const jsClean = (done) => {
+    del([destJsFiles]);
+    done();
+};
+
+// 画像ファイル削除
+const imgClean = (done) => {
+    del([destImgFiles]);
+    done();
+};
+
+// タスク化
+exports.compileEjs = compileEjs;
+exports.compileSass = compileSass;
+exports.compileJs = compileJs;
+exports.reloadFile = reloadFile;
+exports.reloadBrowser = reloadBrowser;
+exports.minifyImage = minifyImage;
+exports.copyMov = copyMov;
+exports.clean = clean;
+exports.htmlClean = htmlClean;
+exports.cssClean = cssClean;
+exports.jsClean = jsClean;
+exports.imgClean = imgClean;
+
+// 監視ファイル
+const watchFiles = (done) => {
+    gulp.watch(srcEjsFiles, gulp.series(htmlClean, compileEjs));
+    gulp.watch(destHtmlFiles, reloadBrowser);
+    gulp.watch(srcScssFiles, gulp.series(cssClean, compileSass));
+    gulp.watch(destCssFiles, reloadBrowser);
+    gulp.watch(srcJsFiles, gulp.series(jsClean, compileJs));
+    gulp.watch(destJsFiles, reloadBrowser);
+    gulp.watch(srcImgFiles, gulp.series(imgClean, minifyImage));
+    gulp.watch(destImgFiles, reloadBrowser);
+    gulp.watch(srcMovFiles, gulp.series(copyMov));
+    gulp.watch(destMovFiles, reloadBrowser);
+    done();
+};
+
+// タスク実行
+exports.default = gulp.series(
+    clean, watchFiles, reloadFile, compileEjs, compileSass, compileJs, minifyImage, copyMov
+);
