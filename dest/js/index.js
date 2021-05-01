6ac575fedb579cfa5ce8200f1c955b4ec94a289c
misoshiru <voushi@icloud.com>
2021-05-01 15:07:55 +0900

    promiseリファクタリング

--- dest/js/index.js
+++ dest/js/index.js
@@ -111,8 +111,8 @@ $(function () {
     });
 
     //アコーディオンをクリックした時の動作
-    $('.title').on('click', function () {//タイトル要素をクリックしたら
-        var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+    $('.accordion-title').on('click', function () {//タイトル要素をクリックしたら
+        var findElm = $(this).next(".accordion-box");//直後のアコーディオンを行うエリアを取得し
         $(findElm).slideToggle();//アコーディオンの上下動作
 
         if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
